52cd18104e88ba890e9a8da755438760b1e184b9
Stewart Lucas <cishcabob123@gmail.com>
2021-05-12 15:59:54 -0700

    Cleaned up CSS a little. Added comments to script.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
-const canvasContainerDiv = document.querySelector('#canvas-container');
-const resetButton = document.querySelector('#reset-button')
-resetButton.addEventListener('click', resetCanvas);
-let canvasSize = 16;
+//ETCH A SKETCH//
 
+const canvasContainerDiv = document.querySelector('#canvas-container');
+//With parameter the square root of canvasSize, initializes the canvas
 function initializeCanvas(canvasSize) {
     let canvasSizeRoot = Math.sqrt(canvasSize);
+
+    //These two initialize the size of the grid
     canvasContainerDiv.style.gridTemplateColumns = `repeat(${canvasSizeRoot}, 
         1fr)`;
     canvasContainerDiv.style.gridTemplateRows = `repeat(${canvasSizeRoot}, 
         1fr)`;
     
+    //Creates the divs in the canvas-container and adds classes and id's
     for (let i = 0; i < canvasSize; i++) {
         let currentCell = document.createElement('div');
         currentCell.id = `${i + 1}`;
@@ -17,21 +19,29 @@ function initializeCanvas(canvasSize) {
         canvasContainerDiv.appendChild(currentCell);
     }
 
+    //Attaches an 'mouseenter' event listener to each div created above
     let canvasCells = document.querySelectorAll('.cell-new');
     canvasCells.forEach(cell => cell.addEventListener('mouseenter', draw));
 }
-   
+
+//Switches the class of 'mouseenter'ed divs from cell-new to cell-drawn (changes color)
 function draw(cell) {
     cell.target.classList.add('cell-drawn')
     cell.target.classList.remove('cell-new');
 }
 
+const resetButton = document.querySelector('#reset-button')
+resetButton.addEventListener('click', resetCanvas);
+//START HERE//
+//Deletes the HTML of the #canvas containter and then calls initialize Canvas again
 function resetCanvas() {
     const canvasListClear = document.querySelector('#canvas-container');
     canvasListClear.innerHTML = '';
-    initializeCanvas(userInputSize());
+    initializeCanvas(16);
 }
 
+//Prompts user to decide canvas edge size
+//Does not accept strings or integers over 100
 function userInputSize() {
     let userCanvasSize = prompt('How many squares would you like per side?');
     console.log(userCanvasSize.typeof);
@@ -44,7 +54,5 @@ function userInputSize() {
     return userCanvasSize;
 }
 
-
-
+//First initialization
 initializeCanvas(36);
-
